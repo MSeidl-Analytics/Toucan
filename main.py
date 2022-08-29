feed17fb69ae456093db7c4c14d2b9fddc41ce4b
MSeidl93 <matthias.seidl@gmx.de>
2022-08-29 15:57:06 +0200

    changed docker stuff

--- main.py
+++ main.py
@@ -92,6 +92,3 @@ app.add_middleware(
 @app.get("/leaderboard", response_model= LeaderboardResponse)
 async def get_path():
     return get_leaderboard()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
